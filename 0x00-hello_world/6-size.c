427d4889b9a807811692c3e0b5d4058d00802fac
gaturuHarry95 <gaturuharrison@gmail.com>
2022-09-08 14:56:48 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,28 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ *main-Entry pont
+ *
+ *Return:always 0(success)
+ */
+
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	char c;
+	int i;
+	long li;
+	long long lli;
 	float f;
 
-	return(0)
+
+
+
+	printf("size of a char: %ld byte(s)\n", size of(c));
+	printf("size of an int: %ld byte(s)\n", size of(i));
+	printf("size of a long int: %ld byte(s)\n", size of(1i));
+	printf("size of a long long int: %ld byte(s)\n", size of(c));
+	printf("size of a float: %id byte(s)\n", size of(f));
+
+
+
+	return (0);
 }
